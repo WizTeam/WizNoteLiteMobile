9186a9e6cd410ef352300c47d642252263eb600b
kjq <kjq@wiz.cn>
2020-08-06 19:11:49 +0800

    android

--- android/app/src/main/java/com/wiznotelitemobile/MainApplication.java
+++ android/app/src/main/java/com/wiznotelitemobile/MainApplication.java
@@ -8,8 +8,11 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import io.liteglue.SQLitePluginPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+            packages.add(new SQLitePluginPackage());
           return packages;
         }
 
