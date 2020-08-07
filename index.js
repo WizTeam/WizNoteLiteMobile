dc6597bcbc47647327cb3cee8df13a9620893e86
wizweishijun <wsj@wiz.cn>
2020-08-07 15:31:07 +0800

    change sdk

--- index.js
+++ index.js
@@ -9,28 +9,4 @@ const wizApi = require('wiznote-sdk-js');
 
 global.wizApi = wizApi;
 
-
-(async () => {
-  try {
-    const allUsers = await wizApi.getUsers()
-    console.log(allUsers);
-    //
-    let user = await wizApi.localLogin();
-    if (!user) {
-      console.log('failed to localLogin, do online login');
-      //
-      user = await users.onlineLogin('as.wiz.cn', 'xxx@xxx.xxx', 'xxxxxx');
-      //
-    } else {
-      console.log('localLogin done');
-      console.log(user);
-    }
-    //
-  } catch (err) {
-    console.error(err);
-  }
-
-})();
-
-
 AppRegistry.registerComponent(appName, () => App);
