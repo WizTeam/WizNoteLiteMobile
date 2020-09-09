2660d1e1f9c295a5a12b608426804503792576aa
wizweishijun <wsj@wiz.cn>
2020-09-09 09:23:04 +0800

    notelistitem

--- components/NoteList.js
+++ components/NoteList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, Dimensions, TouchableOpacity } from 'react-native';
-import { ListItem, Divider } from 'react-native-elements';
 import { DynamicStyleSheet, useDynamicValue } from 'react-native-dynamic';
 import i18n from 'i18n-js';
 import useStateWithCallback from 'use-state-with-callback';
@@ -10,9 +9,8 @@ import { isTablet } from '../utils/device';
 import dataStore from '../data_store';
 import api from '../api';
 import { showTopBarMessage } from '../services/navigation';
-import { formatDateString } from '../utils/date';
-import HighlightText from './HighlightText';
 import { SwipeListView } from '../thirdparty/react-native-swipe-list-view';
+import NoteListItem from './NoteListItem';
 
 const NoteList: () => React$Node = (props) => {
   //
@@ -40,41 +38,14 @@ const NoteList: () => React$Node = (props) => {
     //
     const hideDivider = isTablet && (selected || index === selectedIndex - 1);
     //
-    let title = note.title;
-    let subTitle = formatDateString(note.modified);
-    if (props.showHighlight && note.highlight) {
-      if (note.highlight.title) {
-        const highlightText = note.highlight.title.join(' ');
-        title = <HighlightText text={highlightText} style={styles.title} />;
-      }
-      if (note.highlight.text) {
-        const allText = note.highlight.text.join(' ');
-        const highlightText = `${allText}\n\n${subTitle}`;
-        subTitle = <HighlightText text={highlightText} style={styles.subtitle} />;
-      }
-    }
-    //
-    const showStar = !!(props.showStar && note.starred);
-    //
     return (
-      <View>
-        <ListItem
-          onPress={() => handlerPressItem(note)}
-          key={note.guid}
-          containerStyle={[styles.itemContainer, selected && styles.selected]}
-        >
-          <ListItem.Content style={styles.itemContent}>
-            <ListItem.Title numberOfLines={2} ellipsizeMode="tail" style={styles.title}>
-              <Text>{title}</Text>
-            </ListItem.Title>
-            <ListItem.Subtitle style={styles.subtitle}>{subTitle}</ListItem.Subtitle>
-          </ListItem.Content>
-          {showStar && <ListItem.Chevron name="star" size={20} color="rgb(253, 201, 46)" style={styles.star} />}
-        </ListItem>
-        <View style={styles.dividerContainer}>
-          <Divider style={[styles.divider, hideDivider && styles.hideDivider]} />
-        </View>
-      </View>
+      <NoteListItem
+        note={note}
+        selected={selected}
+        hideDivider={hideDivider}
+        showStar={props.showStar}
+        onPressItem={handlerPressItem}
+      />
     );
   }
   //
